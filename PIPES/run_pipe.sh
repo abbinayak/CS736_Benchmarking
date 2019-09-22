3ddf505ce2ca0647e1f49d2f4305d04032c3500e
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-21 22:49:31 -0500

    Update run_pipe.sh

--- PIPES/run_pipe.sh
+++ PIPES/run_pipe.sh
@@ -15,7 +15,7 @@ cd Throughput
 make
 #Running on royal-10
 #Usage: ./pipe_thr <message_size> 
-#We consider a data block of size 1M and the rounds are calculated based on that
+#We consider a data block of size 1GB and the rounds are calculated based on that
 #The below commands are for 4 bytes
 #The message size to be tested can be specified by changing the first argument
 ./pipe_thr 4
