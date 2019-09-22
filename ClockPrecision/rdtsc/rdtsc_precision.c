27be3e123d7e8d465f44336036fd7cee2747d4c5
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-22 01:42:44 -0500

    Add files via upload

---
+++ ClockPrecision/rdtsc/rdtsc_precision.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "bench.h"
+
+int main(int argc, char** argv)
+{
+	uint high;
+	uint low;
+	uint pid;
+	ull start;
+	ull end;
+	uint val;
+	uint best = (uint)-1;
+	int x;
+
+	for(x = 0;x < 100000;x++)
+	{
+		RDTSC(start);
+		for(int j = 0; j<3; j++){}
+		RDTSC(end);
+			/* Is this the new best? */
+		uint diff = (end - start);
+		if(diff < best) best = diff;
+
+	}
+
+	printf("best run  : %u\n", best);
+	return 0;
+}	
+
