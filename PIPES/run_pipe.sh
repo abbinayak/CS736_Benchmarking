33080b16175c08496fc017ef70986c473b618590
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-21 22:49:18 -0500

    Update run_pipe.sh

--- PIPES/run_pipe.sh
+++ PIPES/run_pipe.sh
@@ -3,18 +3,21 @@ chmod +x run_pipe.sh
 cd Latency
 make
 #Running on royal-10
+#Usage: ./pipe_lat <message_size> <roundtrip_count>
 #The below commands are for 4 bytes
-#The message size to be tested can be specified by changing the argument for -b
-./client -a 128.105.37.150 -p 9010 -b 4
-./server -p 9010 -b 4
+#The message size to be tested can be specified by changing the first argument
+./pipe_lat 4 10000
+
 
 #THROUGHPUT TESTING FOR PIPE
 cd ..
 cd Throughput
 make
 #Running on royal-10
+#Usage: ./pipe_thr <message_size> 
+#We consider a data block of size 1M and the rounds are calculated based on that
 #The below commands are for 4 bytes
-#The message size to be tested can be specified by changing the argument for -b
-./client_thr -a 128.105.37.150 -p 9012 -b 4
-./server_thr -p 9012 -b 4
+#The message size to be tested can be specified by changing the first argument
+./pipe_thr 4
+
 
