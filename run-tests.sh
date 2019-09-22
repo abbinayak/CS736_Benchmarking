43785a68cd72b98c97b0f2c7282f3952464b3b93
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-21 22:53:24 -0500

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,18 +1,18 @@
-//MAIN SCRIPT
- //For TCP
+#MAIN SCRIPT
+ #For TCP
  
 cd TCP/Latency
 make
 
-//Usage: ./client -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
-//Usage: ./server -b <<NUM OF BYTES> -p <PORT NUMBER>
-//Considering 4 bytes of data, that can be changed with the -b flag.
+#Usage: ./client -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
+#Usage: ./server -b <<NUM OF BYTES> -p <PORT NUMBER>
+#Considering 4 bytes of data, that can be changed with the -b flag.
 
-//LOCAL: rockhopper-05
+#LOCAL: rockhopper-05
 ./client -a 128.105.37.195 -b 4 -p 8945
 ./server -b 4 -p 8945
 
-//REMOTE: client: rockhopper-03, server: rockhopper-06
+#REMOTE: client: rockhopper-03, server: rockhopper-06
 ./client -a 128.105.37.196 -b 4 -p 8955
 ./server -b 4 -p 8955
 
@@ -21,36 +21,36 @@ cd ..
 cd TCP/Throughput
 make
 
-//Usage: ./client_thr -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
-//Usage: ./server_thr -b <<NUM OF BYTES> -p <PORT NUMBER>
-//Considering 4 bytes of data, that can be changed with the -b flag.
-//Considering a data block of 1 GB
+#Usage: ./client_thr -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
+#Usage: ./server_thr -b <<NUM OF BYTES> -p <PORT NUMBER>
+#Considering 4 bytes of data, that can be changed with the -b flag.
+#Considering a data block of 1 GB
 
-//LOCAL: rockhopper-05
+#LOCAL: rockhopper-05
 ./client_thr -a 128.105.37.195 -b 4 -p 8965
 ./server_thr -b 4 -p 8965
 
-//REMOTE: client: rockhopper-03, server: rockhopper-06
+#REMOTE: client: rockhopper-03, server: rockhopper-06
 ./client_thr -a 128.105.37.196 -b 4 -p 8975
 ./server_thr -b 4 -p 8975
 
 cd ../..
 
-//For UDP 
+#For UDP 
 
 cd UDP/Latency
 gcc -o udp_client udp_client.c
 gcc -o udp_server udp_server.c
 
-//Usage: ./udp_client -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
-//Usage: ./udp_server -b <<NUM OF BYTES> -p <PORT NUMBER>
-//Considering 4 bytes of data, that can be changed with the -b flag.
+#Usage: ./udp_client -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
+#Usage: ./udp_server -b <<NUM OF BYTES> -p <PORT NUMBER>
+#Considering 4 bytes of data, that can be changed with the -b flag.
 
-//LOCAL: royal-19
+#LOCAL: royal-19
 ./udp_client -a 128.105.37.159 -b 4 -p 8985
 ./udp_server -b 4 -p 8985
 
-//REMOTE: client: snares-02, server: snares-05
+#REMOTE: client: snares-02, server: snares-05
 ./udp_client -a 128.105.37.225 -b 4 -p 8995
 ./udp_server -b 4 -p 8995
 
@@ -60,28 +60,28 @@ cd UDP/Throughput
 gcc -o udp_client_throughput udp_client_throughput.c
 gcc -o udp_server_throughput udp_server_throughput.c
 
-//Usage: ./udp_client_throughput -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
-//Usage: ./udp_server_throughput -b <<NUM OF BYTES> -p <PORT NUMBER>
-//Considering 4 bytes of data, that can be changed with the -b flag.
-//Considering a data block of 1 GB
+#Usage: ./udp_client_throughput -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
+#Usage: ./udp_server_throughput -b <<NUM OF BYTES> -p <PORT NUMBER>
+#Considering 4 bytes of data, that can be changed with the -b flag.
+#Considering a data block of 1 GB
 
-//LOCAL: royal-19
+#LOCAL: royal-19
 ./udp_client_throughput -a 128.105.37.159 -b 4 -p 8985
 ./udp_server_throughput -b 4 -p 8985
 
-//REMOTE: client: snares-02, server: snares-05
+#REMOTE: client: snares-02, server: snares-05
 ./udp_client_throughput -a 128.105.37.225 -b 4 -p 8995
 ./udp_server_throughput -b 4 -p 8995
 
 
-//FOR PIPES
+#FOR PIPES
 cd ../..
 
 cd PIPES/Latency
 gcc -o pipe_lat pipe_lat.c
 
-//Usage: ./pipe_lat <message size> <round trip count>
-//Considering 4 bytes of data, that can be changed here.
+#Usage: ./pipe_lat <message size> <round trip count>
+#Considering 4 bytes of data, that can be changed here.
 
 ./pipe_lat 4 10000
 
@@ -90,8 +90,8 @@ cd ..
 cd PIPES/Throughput
 gcc -o pipe_thr pipe_thr.c
 
-//Usage: ./pipe_thr <message size> 
-//Considering 4 bytes of data, that can be changed here.
-//Considering a data block of 1 GB
+#Usage: ./pipe_thr <message size> 
+#Considering 4 bytes of data, that can be changed here.
+#Considering a data block of 1 GB
 
 ./pipe_thr 4
