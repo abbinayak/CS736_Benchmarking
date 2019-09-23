6b610ae369b5f52610fd162a1f9e04956ae2ea32
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-23 17:11:47 -0500

    Update run_udp.sh

--- UDP/run_udp.sh
+++ UDP/run_udp.sh
@@ -6,16 +6,16 @@ make
 #Local:
 #The below commands are for 4 bytes
 #The message size to be tested can be specified by changing the argument for -b
-./client -a 128.105.37.159 -p 9005 -b 4
 ./server -p 9005 -b 4
+./client -a 128.105.37.159 -p 9005 -b 4
 
 #Remote: Running on snares-02 and snares-05
 #Client: snares-02 and Server snares-05
 #The below commands are for 4 bytes
 #The message size to be tested can be specified by changing the argument for -b
 
-./client -a 128.105.37.225 -p 9006 -b 4
 ./server -p 9006 -b 4
+./client -a 128.105.37.225 -p 9006 -b 4
 
 #THROUGHPUT TESTING FOR UDP LOCAL AND REMOTE
 cd ..
@@ -25,13 +25,13 @@ make
 #Local:
 #The below commands are for 4 bytes
 #The message size to be tested can be specified by changing the argument for -b
-./client_thr -a 128.105.37.159 -p 9007 -b 4
 ./server_thr -p 9007 -b 4
+./client_thr -a 128.105.37.159 -p 9007 -b 4
 
 #Remote: Running on snares-02 and snares-05
 #Client: snares-02 and Server snares-05
 #The below commands are for 4 bytes
 #The message size to be tested can be specified by changing the argument for -b
 
-./client_thr -a 128.105.37.225 -p 9008 -b 4
 ./server_thr -p 9008 -b 4
+./client_thr -a 128.105.37.225 -p 9008 -b 4
