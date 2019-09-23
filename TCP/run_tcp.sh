e02aa91e66a4c863a6f75279a7e3e4c9114e53a3
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-23 17:12:29 -0500

    Update run_tcp.sh

--- TCP/run_tcp.sh
+++ TCP/run_tcp.sh
@@ -6,16 +6,16 @@ make
 #Local:
 #The below commands are for 4 bytes
 #The message size to be tested can be specified by changing the argument for -b
-./client -a 128.105.37.195 -p 9005 -b 4
 ./server -p 9005 -b 4
+./client -a 128.105.37.195 -p 9005 -b 4
 
 #Remote: Running on rockhopper-03 and rockhopper-06
 #Client: rockhopper-03 and Server rockhopper-06
 #The below commands are for 4 bytes
 #The message size to be tested can be specified by changing the argument for -b
 
-./client -a 128.105.37.196 -p 9005 -b 4
 ./server -p 9005 -b 4
+./client -a 128.105.37.196 -p 9005 -b 4
 
 #THROUGHPUT TESTING FOR TCP LOCAL AND REMOTE
 cd ..
@@ -25,13 +25,13 @@ make
 #Local:
 #The below commands are for 4 bytes
 #The message size to be tested can be specified by changing the argument for -b
-./client_thr -a 128.105.37.195 -p 9006 -b 4
 ./server_thr -p 9006 -b 4
+./client_thr -a 128.105.37.195 -p 9006 -b 4
 
 #Remote: Running on rockhopper-03 and rockhopper-06
 #Client: rockhopper-03 and Server rockhopper-06
 #The below commands are for 4 bytes
 #The message size to be tested can be specified by changing the argument for -b
 
-./client_thr -a 128.105.37.196 -p 9006 -b 4
 ./server_thr -p 9006 -b 4
+./client_thr -a 128.105.37.196 -p 9006 -b 4
