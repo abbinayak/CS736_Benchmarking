f0bdc98a4ec36292c02d4e778e33540dc7c49831
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:10:12 -0500

    Update udp_server_local.sh

--- UDP/Latency/udp_server_local.sh
+++ UDP/Latency/udp_server_local.sh
@@ -3,5 +3,5 @@ gcc -o udp_server udp_server.c
 #Usage: ./udp_server -b <<NUM OF BYTES> -p <PORT NUMBER>
 #Considering 4 bytes of data, that can be changed with the -b flag.
 
-#REMOTE: snares - 05
+#LOCAL: snares - 02
 ./udp_server -b 4 -p 8985
