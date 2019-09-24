3e1dc007c74897427492958eea06acf56a368f58
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:27:57 -0500

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -4,9 +4,13 @@ chmod +x run-tests.sh
 #Set permissions
 fs setacl TCP/Latency bart rlidw
 fs setacl TCP/Throughput bart rlidw
+
 fs setacl UDP/Latency bart rlidw
 fs setacl UDP/Throughput bart rlidw
+
+fs setacl ClockPrecision bart rlidw
 fs setacl ClockPrecision/clock bart rlidw
+fs setacl ClockPrecision/clock_gettime bart rlidw
 fs setacl ClockPrecision/rdtsc bart rlidw
-fs setacl ClockPrecision/clockgettime bart rlidw
+
 fs setacl PIPES bart rlidw
