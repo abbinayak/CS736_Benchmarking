5e481642f8a89a3614e760a2ff3f5e0d7a895b5a
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 11:51:19 -0500

    Update tcp_client_local.sh

--- TCP/Throughput/tcp_client_local.sh
+++ TCP/Throughput/tcp_client_local.sh
@@ -1,1 +1,10 @@
 make
+#Usage: ./client_thr -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
+#Usage: ./server_thr -b <<NUM OF BYTES> -p <PORT NUMBER>
+#Considering 4 bytes of data, that can be changed with the -b flag.
+#Considering a data block of 1 GB
+
+#LOCAL: rockhopper-05
+#Open 2 terminals of rockhopper-05, one for the server and one for the client
+./server_thr -b 4 -p 8965
+./client_thr -a 128.105.37.195 -b 4 -p 8965
