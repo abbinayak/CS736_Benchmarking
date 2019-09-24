6c48df89349f3ce777bb60602eafaa121deaea9b
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 14:12:19 -0500

    Update clock_precision.c

--- ClockPrecision/clock/clock_precision.c
+++ ClockPrecision/clock/clock_precision.c
@@ -23,11 +23,19 @@ int main()
 	{
 		/* Recording the starting clock tick.*/
 		start = clock(); 
-
+		if(start==-1){
+			perror("Clock error on start");
+			exit(1);
+		}
+		
 		fun(); 
 
 		// Recording the end clock tick. 
 		end = clock(); 
+		if(end==-1){
+			perror("Clock error on end");
+			exit(1);
+		}
 
 		// Calculating total time taken by the program. 
 		double time_taken = (double)(end - start) / (double)(CLOCKS_PER_SEC);  
