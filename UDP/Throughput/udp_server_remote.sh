95ae94e526a85fbf7213ff32596ab7c9c62785dc
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:11:04 -0500

    Create udp_server_remote.sh

---
+++ UDP/Throughput/udp_server_remote.sh
@@ -0,0 +1,7 @@
+gcc -o udp_server_throughput udp_server_throughput.c
+
+#Usage: ./udp_server_throughput -b <<NUM OF BYTES> -p <PORT NUMBER>
+#Considering 4 bytes of data, that can be changed with the -b flag.
+
+#REMOTE: snares - 05
+./udp_server_throughput -b 4 -p 8995
