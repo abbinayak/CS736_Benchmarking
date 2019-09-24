17e45c14d65a08c8cbe53ce3bbce7084ef677221
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 14:16:25 -0500

    Update clock_gettime.c

--- ClockPrecision/clock_gettime/clock_gettime.c
+++ ClockPrecision/clock_gettime/clock_gettime.c
@@ -7,9 +7,17 @@ int main()
 	struct timespec start,end;
 	double best=1000000,diff;
 	for(int i=1;i<100000000;i++){
-		clock_gettime(CLOCK_MONOTONIC, &start);
+		if(clock_gettime(CLOCK_MONOTONIC, &start)==-1)
+		{
+			perror("Error on clock gettime start);
+			exit(1);
+		}
 		for(int j=1;j<3;j++){}
-		clock_gettime(CLOCK_MONOTONIC, &end);
+		if(clock_gettime(CLOCK_MONOTONIC, &end)==-1)
+		{
+			perror("Error on clock gettime end);
+			exit(1);
+		}
 		diff = (end.tv_sec - start.tv_sec)*(10^9);
 		diff+= (end.tv_nsec - start.tv_nsec);
 		if(diff < best && diff>0) best = diff;
