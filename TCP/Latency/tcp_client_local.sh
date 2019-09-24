5c40f28ea7abae1ea0b7bd358dbed3a68418eac3
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 11:49:57 -0500

    Update tcp_client_local.sh

--- TCP/Latency/tcp_client_local.sh
+++ TCP/Latency/tcp_client_local.sh
@@ -1,1 +1,9 @@
-hi
+chmod +x tcp_client_local.sh
+#LATENCY TESTING FOR TCP LOCAL AND REMOTE
+make
+#Running on rockhopper-05
+#Local:
+#The below commands are for 4 bytes
+#The message size to be tested can be specified by changing the argument for -b
+./client -a 128.105.37.195 -p 9005 -b 4
+
