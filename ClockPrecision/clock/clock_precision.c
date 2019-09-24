05ea66d694ff0abe497a4c0ea4123383558e4495
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:44:02 -0500

    Update clock_precision.c

--- ClockPrecision/clock/clock_precision.c
+++ ClockPrecision/clock/clock_precision.c
@@ -33,6 +33,6 @@ int main()
 		double time_taken = (double)(end - start) / (double)(CLOCKS_PER_SEC);  
 		if(time_taken < best) best = time_taken;
 	}
-	printf("Time taken by program is : %f nanoseconds\n", best*BILLION) ;  
+	printf("Time taken by clock() is : %f nanoseconds\n", best*BILLION) ;  
 	return 0;
 } 
