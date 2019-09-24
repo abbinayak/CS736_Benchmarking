d79aa28c9bfdb516de0e6bd5e0be5f729bb19d9e
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 11:55:40 -0500

    Create tcp_server_remote.sh

---
+++ TCP/Throughput/tcp_server_remote.sh
@@ -0,0 +1,9 @@
+make
+#Usage: ./server_thr -b <NUM OF BYTES> -p <PORT NUMBER>
+
+#Considering 4 bytes of data, that can be changed with the -b flag.
+#Considering a data block of 1 GB
+
+#LOCAL: server - rockhopper-06
+
+./server_thr -b 4 -p 8975
