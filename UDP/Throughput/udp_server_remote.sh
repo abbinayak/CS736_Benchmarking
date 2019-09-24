960fc00a3b1a12c828e48f6441142aae6c6a448c
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:15:34 -0500

    Update udp_server_remote.sh

--- UDP/Throughput/udp_server_remote.sh
+++ UDP/Throughput/udp_server_remote.sh
@@ -3,5 +3,5 @@ gcc -o udp_server_throughput udp_server_throughput.c
 #Usage: ./udp_server_throughput -b <<NUM OF BYTES> -p <PORT NUMBER>
 #Considering 4 bytes of data, that can be changed with the -b flag.
 
-#REMOTE: snares - 05
+#REMOTE: client in snares - 02 connecting to server in snares - 05
 ./udp_server_throughput -b 4 -p 8995
