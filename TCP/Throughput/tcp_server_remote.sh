2ad62cacb95c8bf891710ac745869f058592ff8b
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:04:53 -0500

    Update tcp_server_remote.sh

--- TCP/Throughput/tcp_server_remote.sh
+++ TCP/Throughput/tcp_server_remote.sh
@@ -2,9 +2,12 @@ chmod +x tcp_server_remote.sh
 make
 #Usage: ./server_thr -b <NUM OF BYTES> -p <PORT NUMBER>
 
-#Considering 4 bytes of data, that can be changed with the -b flag.
-#Considering a data block of 1 GB
+#Remote: Running on rockhopper-05 and rockhopper-06
+#Client: rockhopper-05 and Server rockhopper-06
 
-#REMOTE: server - rockhopper-06
+#The below commands are for 4 bytes message size
+#The message size to be tested can be specified by changing the argument for -b
+#Considering a data block of 1 GB in the code
 
 ./server_thr -b 4 -p 8975
+
