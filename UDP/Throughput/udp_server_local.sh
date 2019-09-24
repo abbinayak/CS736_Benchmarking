efee977d4136618af2b26b4862badbd40d54ccc4
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:10:00 -0500

    Update udp_server_local.sh

--- UDP/Throughput/udp_server_local.sh
+++ UDP/Throughput/udp_server_local.sh
@@ -3,5 +3,5 @@ gcc -o udp_server_throughput udp_server_throughput.c
 #Usage: ./udp_server_throughput -b <<NUM OF BYTES> -p <PORT NUMBER>
 #Considering 4 bytes of data, that can be changed with the -b flag.
 
-#REMOTE: snares - 05
+#LOCAL: snares - 02
 ./udp_server_throughput -b 4 -p 8985
