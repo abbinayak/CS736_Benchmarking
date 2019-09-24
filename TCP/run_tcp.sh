a83aba43bce0d5881da28f96aaac6b4b7e5e596d
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 11:43:49 -0500

    Update run_tcp.sh

--- TCP/run_tcp.sh
+++ TCP/run_tcp.sh
@@ -9,8 +9,8 @@ make
 ./server -p 9005 -b 4
 ./client -a 128.105.37.195 -p 9005 -b 4
 
-#Remote: Running on rockhopper-03 and rockhopper-06
-#Client: rockhopper-03 and Server rockhopper-06
+#Remote: Running on rockhopper-05 and rockhopper-06
+#Client: rockhopper-05 and Server rockhopper-06
 #The below commands are for 4 bytes
 #The message size to be tested can be specified by changing the argument for -b
 
@@ -28,8 +28,8 @@ make
 ./server_thr -p 9006 -b 4
 ./client_thr -a 128.105.37.195 -p 9006 -b 4
 
-#Remote: Running on rockhopper-03 and rockhopper-06
-#Client: rockhopper-03 and Server rockhopper-06
+#Remote: Running on rockhopper-05 and rockhopper-06
+#Client: rockhopper-05 and Server rockhopper-06
 #The below commands are for 4 bytes
 #The message size to be tested can be specified by changing the argument for -b
 
