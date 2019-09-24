ce3aabe312287e058f2b53632fb0cae4d1c867ff
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:12:19 -0500

    Update tcp_server_local.sh

--- TCP/Latency/tcp_server_local.sh
+++ TCP/Latency/tcp_server_local.sh
@@ -1,10 +1,10 @@
 chmod +x tcp_server_local.sh
 #LATENCY TESTING FOR TCP LOCAL
 make
+#Usage: ./server -b <NUM OF BYTES> -p <PORT NUMBER>
+
 #Running on rockhopper-05
 #Local:
-#The below commands are for 4 bytes
+#The below commands considers 4 bytes message size
 #The message size to be tested can be specified by changing the argument for -b
 ./server -p 9005 -b 4
-
-
