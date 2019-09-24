714850c18f2725049e2154c1fdae7f21080d20ee
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:11:15 -0500

    Update tcp_client_remote.sh

--- TCP/Latency/tcp_client_remote.sh
+++ TCP/Latency/tcp_client_remote.sh
@@ -1,11 +1,13 @@
 chmod +x tcp_client_remote.sh
 #LATENCY TESTING FOR TCP REMOTE
 make
+#Usage: ./client_thr -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
+
 #Remote: Running on rockhopper-05 and rockhopper-06
 #Client: rockhopper-05 and Server rockhopper-06
-#The below commands are for 4 bytes
-#The message size to be tested can be specified by changing the argument for -b
 
+#The below command considers 4 bytes message size
+#The message size to be tested can be specified by changing the argument for -b
 
 ./client -a 128.105.37.196 -p 9005 -b 4
 
