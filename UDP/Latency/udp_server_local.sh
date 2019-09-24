780153509f7850dac16d449292a8c0c33103c721
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:01:32 -0500

    Create udp_server_local.sh

---
+++ UDP/Latency/udp_server_local.sh
@@ -0,0 +1,6 @@
+gcc -o udp_server udp_server.c
+
+#Usage: ./udp_server -b <<NUM OF BYTES> -p <PORT NUMBER>
+#Considering 4 bytes of data, that can be changed with the -b flag.
+
+./udp_server -b 4 -p 8985
