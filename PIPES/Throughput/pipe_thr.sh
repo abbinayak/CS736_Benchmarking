0a419ee5216354c3e3af459586688975bb651de6
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:17:56 -0500

    Create pipe_thr.sh

---
+++ PIPES/Throughput/pipe_thr.sh
@@ -0,0 +1,13 @@
+#THROUGHPUT TESTING FOR PIPE
+gcc pipe_thr.c -o pipe_thr
+
+#Running on royal-10
+#Usage: ./pipe_thr <message_size> 
+
+#We consider a data block of size 1GB and the rounds are calculated based on that
+#The below command considers 4 bytes message size
+#The message size to be tested can be specified by changing the first argument
+
+./pipe_thr 4
+
+
