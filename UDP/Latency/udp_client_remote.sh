fe51b5f78e8bd1045a04089122e889103aa12786
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:02:42 -0500

    Create udp_client_remote.sh

---
+++ UDP/Latency/udp_client_remote.sh
@@ -0,0 +1,8 @@
+gcc -o udp_client udp_client.c
+
+#Usage: ./udp_client -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
+
+#Considering 4 bytes of data, that can be changed with the -b flag.
+
+#LOCAL: snares - 02
+./udp_client -a 128.105.37.225 -b 4 -p 8995
