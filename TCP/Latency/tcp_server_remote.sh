2b7f64c1cba17bcf4eb612d0ab9458ad1d8e32a0
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 11:54:28 -0500

    Create tcp_server_remote.sh

---
+++ TCP/Latency/tcp_server_remote.sh
@@ -0,0 +1,10 @@
+chmod +x tcp_server_remote.sh
+#LATENCY TESTING FOR TCP REMOTE
+make
+#Remote: Running on rockhopper-05 and rockhopper-06
+#Client: rockhopper-05 and Server rockhopper-06
+#The below commands are for 4 bytes
+#The message size to be tested can be specified by changing the argument for -b
+
+./server -p 9005 -b 4
+
