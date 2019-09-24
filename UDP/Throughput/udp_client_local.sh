b9d043ff68e247e317e80b675ce5caae9ca49e3f
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:06:08 -0500

    Create udp_client_local.sh

---
+++ UDP/Throughput/udp_client_local.sh
@@ -0,0 +1,8 @@
+gcc -o udp_client_throughput udp_client_throughput.c
+
+#Usage: ./udp_client_throughput -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
+
+#Considering 4 bytes of data, that can be changed with the -b flag.
+
+#LOCAL: snares - 02
+./udp_client_throughput -a 128.105.37.222 -b 4 -p 8985
