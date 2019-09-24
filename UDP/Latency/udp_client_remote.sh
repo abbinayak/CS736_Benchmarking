ee10faebed8763cb1ead32e8d16b521fffb36ee0
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 12:07:28 -0500

    Update udp_client_remote.sh

--- UDP/Latency/udp_client_remote.sh
+++ UDP/Latency/udp_client_remote.sh
@@ -4,5 +4,5 @@ gcc -o udp_client udp_client.c
 
 #Considering 4 bytes of data, that can be changed with the -b flag.
 
-#LOCAL: snares - 02
+#REMOTE: snares - 02, connecting to server in snares - 05
 ./udp_client -a 128.105.37.225 -b 4 -p 8995
