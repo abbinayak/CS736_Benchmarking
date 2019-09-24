e5f83fce683083ea93332bb4475ec6b8228d605d
abbinayak <45108728+abbinayak@users.noreply.github.com>
2019-09-24 11:54:46 -0500

    Create tcp_client_remote.sh

---
+++ TCP/Throughput/tcp_client_remote.sh
@@ -0,0 +1,9 @@
+make
+#Usage: ./client_thr -a <IP-ADDRESS> -b <NUM OF BYTES> -p <PORT NUMBER>
+
+#Considering 4 bytes of data, that can be changed with the -b flag.
+#Considering a data block of 1 GB
+
+#REMOTE: client - rockhopper-05
+
+./client_thr -a 128.105.37.196 -b 4 -p 8975
